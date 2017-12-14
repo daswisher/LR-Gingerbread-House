706b92f8d895175cbdad60343bd506c826efb703
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-12-13 17:55:42 -0700

    Updated main controller to handle multiple rgb led lines

--- maincontroller.py
+++ maincontroller.py
@@ -14,26 +14,38 @@ GPIO.setwarnings(False)
 # LED setup
 colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0xFFFFFF, 0x9400D3]
 
-ledPins = {'redPin':26, 'greenPin':19, 'bluePin':13}
+configuredLines = {
+	'line1':{'redPinNum':13, 'greenPinNum':19, 'bluePinNum':26},
+	'line2':{'redPinNum':16, 'greenPinNum':4, 'bluePinNum':21},
+	'line3':{'redPinNum':17, 'greenPinNum':27, 'bluePinNum':22}
+}
 
+for ledLine in configuredLines:
+	ledPins = configuredLines[ledLine]
+	for i in ledPins:
+		print i, ledPins[i]
+		GPIO.setup(ledPins[i], GPIO.OUT)
+		GPIO.output(ledPins[i], GPIO.HIGH)
 
-for i in ledPins:
-	print i, ledPins[i]
-	GPIO.setup(ledPins[i], GPIO.OUT)
-	GPIO.output(ledPins[i], GPIO.HIGH)
+	redPin = GPIO.PWM(ledPins['redPinNum'], 2000)
+	greenPin = GPIO.PWM(ledPins['greenPinNum'], 2000)
+	bluePin = GPIO.PWM(ledPins['bluePinNum'], 2000)
+	redPin.start(0)
+	greenPin.start(0)
+	bluePin.start(0)
 
-redPin = GPIO.PWM(ledPins['redPin'], 2000)
-greenPin = GPIO.PWM(ledPins['greenPin'], 2000)
-bluePin = GPIO.PWM(ledPins['bluePin'], 2000)
-redPin.start(0)
-greenPin.start(0)
-bluePin.start(0)
+	configuredLines[ledLine]['redPin'] = redPin
+	configuredLines[ledLine]['greenPin'] = greenPin
+	configuredLines[ledLine]['bluePin'] = bluePin 
 
 # Sonar sensor setup
 echoPin = 20 #It's actually 20
 triggerPin = 21 #It's actually 21
 GPIO.setup(echoPin, GPIO.IN)
 GPIO.setup(triggerPin, GPIO.OUT)
+
+# Servo setup
+servoPin = 222
 
 ###################################################
 # Functions
@@ -42,7 +54,10 @@ GPIO.setup(triggerPin, GPIO.OUT)
 def map(x, in_min, in_max, out_min, out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def setColor(col):
+def setColor(col, configuredLine):
+	r_pin = configuredLine['redPin']
+	b_pin = configuredLine['bluePin']
+	g_pin = configuredLine['greenPin']
 	redVal = (col & 0x110000) >> 16
 	blueVal = (col & 0x001100) >> 8
 	greenVal = (col & 0x000011) >> 0
@@ -58,9 +73,9 @@ def setColor(col):
 	#bluePin.ChangeDutyCycle(100-blueVal)
 	#greenPin.ChangeDutyCycle(100-greenVal)
 
-	redPin.ChangeDutyCycle(redVal)
-	bluePin.ChangeDutyCycle(blueVal)
-	greenPin.ChangeDutyCycle(greenVal)
+	r_pin.ChangeDutyCycle(redVal)
+	b_pin.ChangeDutyCycle(blueVal)
+	g_pin.ChangeDutyCycle(greenVal)
 
 def distance():
 	GPIO.output(triggerPin, GPIO.LOW)
@@ -102,8 +117,9 @@ def distance():
 try:
 	while True:
 		for col in colors:
-			setColor(col)
-			time.sleep(1.0)
+			for lineName in configuredLines:
+				setColor(col, configuredLines[lineName])
+				time.sleep(1.0)
 
 except KeyboardInterrupt:
 	redPin.stop()
